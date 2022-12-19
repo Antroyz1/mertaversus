15553fafe0573a8a27fefb6f599e7b3adda04c72
Antroyz1 <roymond.chandra93@gmail.com>
2022-12-20 05:52:59 +0700

    complete

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,7 +7,7 @@ import styles from "../styles";
 import { footerVariants } from "../utils/motion";
 
 const Footer = () => (
-  <motion.footer variants={footerVariants} initial="hidden" whileInView="show" className={`${styles.paddings} py-8 relative`}>
+  <motion.footer variants={footerVariants} initial="hidden" whileInView="show" className={`${styles.paddings} py-8 relative`} >
     <div className="footer-gradient" />
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
       <div className="flex items-center justify-between flex-wrap gap-5">
@@ -21,7 +21,7 @@ const Footer = () => (
         <div className="mb-[50px] h-[2px] bg-white opacity-10" />
         <div className="flex items-center justify-between flex-wrap gap-4">
           <h4 className="font-extrabold text-[24px] text-white">Metaversus</h4>
-          <p className="font-normal text-[14px] text-white opacity-50">Copyright © 2021 - 2022 Metaversus. All rights reserved.</p>
+          <p className="font-normal text-[14px] text-white opacity-50">Copyright © 2021 - 2022 Metaversus recreated by Roymond Chandra Pradana. All rights reserved.</p>
           <div className="flex gap-4">
             {socials.map((social) => (
               <img key={social.name} src={social.url} alt={social.name} className="w-[24px] h-[24px] object-contain cursor-pointer" />
