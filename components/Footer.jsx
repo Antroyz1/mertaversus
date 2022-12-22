e5e08f9605c7594dc8ee04812847df11f9ffd135
Antroyz1 <roymond.chandra93@gmail.com>
2022-12-22 14:47:48 +0700

    Elsint bug fixed

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,15 @@
-"use client";
+/* eslint-disable react/button-has-type */
 
-import { motion } from "framer-motion";
-import { socials } from "../constants";
+'use client';
 
-import styles from "../styles";
-import { footerVariants } from "../utils/motion";
+import { motion } from 'framer-motion';
+import { socials } from '../constants';
+
+import styles from '../styles';
+import { footerVariants } from '../utils/motion';
 
 const Footer = () => (
-  <motion.footer variants={footerVariants} initial="hidden" whileInView="show" className={`${styles.paddings} py-8 relative`} >
+  <motion.footer variants={footerVariants} initial="hidden" whileInView="show" className={`${styles.paddings} py-8 relative`}>
     <div className="footer-gradient" />
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
       <div className="flex items-center justify-between flex-wrap gap-5">
